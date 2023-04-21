8dd5c6c68a1ce8a275c39e62e88f63cae9ea24f4
Alexey Zatonskiy <lehazm.z@gmail.com>
2023-04-21 23:59:10 +0300

    add postgresql connection

--- src/workshop/settings/settings.py
+++ src/workshop/settings/settings.py
@@ -1,18 +1,23 @@
 from pydantic import BaseSettings
 
+from dotenv import load_dotenv
+import os
 
+
+
+load_dotenv()
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+
+SERVER_HOST = os.environ.get('SERVER_HOST')
+SERVER_PORT = os.environ.get('SERVER_PORT')
 
 
 class Settings(BaseSettings):
-    server_host: str = '127.0.0.1'
-    server_port: int = 8080
-    database_url = 'sqlite:///./database.db'
-
-
-
-'''
-Valid SQLite URL forms are:
-sqlite:///:memory: (or, sqlite://)
-sqlite:///relative/path/to/file.db
-sqlite:////absolute/path/to/file.db
-'''
+    server_host: str = SERVER_HOST
+    server_port: int = SERVER_PORT
+    database_url = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
