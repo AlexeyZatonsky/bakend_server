574e6089ba601918ca16ca2f75f028cff368376e
Alexey Zatonskiy <lehazm.z@gmail.com>
2023-05-12 05:47:42 +0300

    create the basis for the class

--- src/videos/services.py
+++ src/videos/services.py
@@ -2,3 +2,19 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import  AsyncSession
 
 from .models import Video, Tag, Category, video_tag
+from .schemas import ReadCategory, CreateCategory, \
+ReadTag, CreateTag, UploadVideo, AboutVideo, BaseVideoTag
+
+from ..database import get_async_session
+
+from ..auth.base_config import current_user
+
+
+
+
+class OperationsVideo:
+    def __init__(self,
+                 session: AsyncSession=(Depends(get_async_session))):
+        self.session = session
+    
+    
