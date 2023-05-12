cdc9401f071784ea9e0b30c3743950b7c253b2d0
Alexey Zatonskiy <lehazm.z@gmail.com>
2023-05-12 06:11:19 +0300

    blank for functions services

--- src/videos/services.py
+++ src/videos/services.py
@@ -2,19 +2,48 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import  AsyncSession
 
 from .models import Video, Tag, Category, video_tag
-from .schemas import ReadCategory, CreateCategory, \
-ReadTag, CreateTag, UploadVideo, AboutVideo, BaseVideoTag
+from .schemas import CategoryRead, CategoryCreate, \
+TagRead, TagCreate, VidoeUpload, AboutVideo, BaseVideoTag
 
 from ..database import get_async_session
 
 from ..auth.base_config import current_user
+from ..auth.models import User
 
 
 
 
 class OperationsVideo:
     def __init__(self,
-                 session: AsyncSession=(Depends(get_async_session))):
+                 session: AsyncSession=(Depends(get_async_session)),
+                 current_user: User=Depends(current_user)):
         self.session = session
+        self.current_user = current_user
     
-    
+    async def video_upload(self, video_data: VidoeUpload) -> Video:
+        pass
+
+    async def video_read_about(self, video_data: AboutVideo):
+        pass
+
+    async def video_remove(self): pass
+
+
+
+    async def category_create(self, category_data: CategoryCreate) -> Category:
+        pass
+
+    async def category_read(self, category_data: CategoryRead):
+        pass
+
+    async def category_remove(self): pass
+
+
+
+    async def tag_create(self, tag_data: TagCreate) -> Tag:
+        pass  
+
+    async def tag_read(self, tag_data: TagRead):
+        pass
+
+    async def tag_remove(self): pass
