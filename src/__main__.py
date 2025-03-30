302818f094cb0550f62e10033105a4fb6d96b539
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-03-30 20:21:07 +0300

    Потихоньку добавляю логирование

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,13 @@
 import uvicorn
+import logging
 
 from .settings.config import settings
                           
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 
+)
 
 if __name__ == '__main__':
 
