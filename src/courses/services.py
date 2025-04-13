f7f5aef80168eecede8ad6dc0dbdd51d3843e4f4
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-04-13 18:25:16 +0300

    подправлен метод создания курса

--- src/courses/services.py
+++ src/courses/services.py
@@ -3,6 +3,8 @@ from uuid import UUID
 
 from fastapi import HTTPException, status
 
+from ..auth.schemas import UserReadSchema
+
 from ..courses.models import CoursesORM
 from ..channels.schemas import ChannelReadSchema
 
@@ -24,25 +26,28 @@ class CourseService:
     async def create(
             self, 
             course_data: CourseCreateSchema, 
-            channel: ChannelReadSchema
+            channel_data: ChannelReadSchema
         ) -> CourseReadSchema | HTTPException:
 
-        if await self.repository.get_by_channel_id(channel.id) is not None:
+        if await self.repository.get_by_name_and_channel_id(channel_data.id, course_data.name) is not None:
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail="There's already a course with that name on your channel."
             )
         
-        new_course = CoursesORM(**course_data.model_dump(), channel_id=channel.id)
-
+        #TODO: подключение aws - создавать директорию для курса.
+        
+        new_course = CoursesORM(**course_data.model_dump(), channel_id=channel_data.id)
         saved_course = await self.repository.create(new_course)
-
-        return CourseReadSchema.model_validate()
+        return CourseReadSchema.model_validate(saved_course)
 
     async def get_all(self, limit: int = 20) -> List[CourseReadSchema]:
         courses = await self.repository.get_all(limit)
         return [CourseReadSchema.model_validate(course) for course in courses]
+    
+    async def get_courses_by_cahnnel(self, channel_data : ChannelReadSchema) -> List[CourseReadSchema]: pass
 
+    async def delete_course(self, user_data : UserReadSchema, channel_data : ChannelReadSchema, course_id : UUID)  : pass
 
 
 
