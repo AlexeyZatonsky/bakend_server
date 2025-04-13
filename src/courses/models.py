2993d16910f578cd2b036348256151980cc73151
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-04-13 18:23:04 +0300

    Добавлено поле is_public для курса

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime, UTC
 
-from sqlalchemy import String, Integer, ForeignKey, DateTime
+from sqlalchemy import Boolean, String, Integer, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -16,6 +16,7 @@ class CoursesORM(Base):
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     channel_id: Mapped[str] = mapped_column(String, ForeignKey(ChannelsORM.id, ondelete='CASCADE'))
     name: Mapped[str] = mapped_column(String(255))
+    is_public: Mapped[bool] = mapped_column(Boolean, default=True)
     student_count: Mapped[int] = mapped_column(Integer, default=0)
     preview: Mapped[str | None] = mapped_column(String(1000), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
