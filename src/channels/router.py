e2f0720cae05f6447cfc8bbdd1fe7051f02da09f
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-04-14 17:37:05 +0300

    rerout

--- src/channels/router.py
+++ src/channels/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
+
 from uuid import UUID
 
-from ..database import get_async_session
 from ..auth.dependencies import get_current_user
 from ..auth.schemas import UserReadSchema
 
+from .dependencies import get_channel_service
 from .schemas import ChannelCreateSchema, ChannelReadSchema
 from .repository import ChannelRepository
 from .service import ChannelService
@@ -15,9 +15,6 @@ router = APIRouter(
     tags=["Channels"]
 )
 
-async def get_channel_service(session: AsyncSession = Depends(get_async_session)):
-    repository = ChannelRepository(session)
-    return ChannelService(repository)
 
 @router.post("", response_model=ChannelReadSchema, status_code=status.HTTP_201_CREATED)
 async def create_channel(
@@ -37,8 +34,6 @@ async def get_channels(channel_service: ChannelService = Depends(get_channel_ser
     """
     return await channel_service.get_channels()
 
-# Внимание: порядок маршрутов имеет значение!
-# Более специфичные маршруты должны идти перед общими
 
 @router.get("/my", response_model=list[ChannelReadSchema])
 async def get_my_channels(
@@ -57,20 +52,18 @@ async def get_user_channels(
     """Получает все каналы указанного пользователя"""
     return await channel_service.get_user_channels(owner_id)
 
-
-# Общий маршрут для получения канала по имени должен идти после специфичных маршрутов
-@router.get("/{unique_name}", response_model=ChannelReadSchema)
+@router.get("/{channel_id}", response_model=ChannelReadSchema)
 async def get_channel(
-    unique_name: str,
+    channel_id: str,
     channel_service: ChannelService = Depends(get_channel_service)
 ):
     """Получает канал по его уникальному имени"""
-    return await channel_service.get_channel_by_name(unique_name)
+    return await channel_service.get_channel_by_name(channel_id)
 
 
-@router.delete("/{unique_name}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{channel_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_channel(
-    unique_name: str,
+    channel_id: str,
     current_user: UserReadSchema = Depends(get_current_user),
     channel_service: ChannelService = Depends(get_channel_service)
 ):
@@ -84,7 +77,7 @@ async def delete_channel(
     Raises:
         404: Канал не найден или пользователь не является владельцем
     """
-    deleted = await channel_service.delete_channel(unique_name, current_user)
+    deleted = await channel_service.delete_channel(channel_id, current_user)
     if not deleted:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
--- src/courses/router.py
+++ src/courses/router.py
@@ -1,8 +1,7 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 
-from ..database import get_async_session
 
 from ..auth.dependencies import get_current_user
 from ..auth.schemas import UserReadSchema
@@ -10,9 +9,11 @@ from ..auth.schemas import UserReadSchema
 from ..channels.service import ChannelService
 from ..channels.repository import ChannelRepository
 from ..channels.schemas import ChannelReadSchema
+from ..channels.dependencies import get_channel_service
 
-from .repository import CourseRepository, CourseStructureRepository
-from .services import CourseService, CourseStructureService
+from .dependencies import get_course_service, get_course_structure_service
+from .service import CourseService
+
 
 from .schemas import (
     CourseCreateSchema, CourseUpdateSchema, CourseReadSchema
@@ -21,25 +22,32 @@ from .schemas import (
 )
 
 router = APIRouter(
-    prefix="/channels/{channel_id}/courses",
     tags=["Courses"]
 )
 
-def get_course_service(session: AsyncSession = Depends(get_async_session)) -> CourseService:
-    repository = CourseRepository(session)
-    return CourseService(repository)
-
-def get_course_structure_service(session: AsyncSession = Depends(get_async_session)) -> CourseStructureService:
-    repository = CourseStructureRepository(session)
-    return CourseStructureService(repository)
 
 
-def get_channel_service(session: AsyncSession = Depends(get_async_session)) -> ChannelService:
-    repository = ChannelRepository(session)
-    return ChannelService(repository)
+@router.get("/courses", response_model=List[CourseReadSchema])
+async def get_all_courses(
+    course_service: CourseService = Depends(get_course_service)
+):
+    return await course_service.get_all_public_courses()
 
 
-@router.post("", response_model=CourseReadSchema, status_code=status.HTTP_201_CREATED)
+@router.get("/courses/{course_id}", response_model=CourseReadSchema)
+async def get_course_by_id(course_id : UUID): pass
+
+@router.get("/users/{user_id}/channels/courses", response_model=List[CourseReadSchema])
+async def get_user_courses(
+    user_id: UUID,
+    course_service: CourseService = Depends(get_course_service),
+    channel_service: ChannelService = Depends(get_channel_service)
+):
+    return await course_service.get_courses_by_user(user_id, channel_service)
+
+
+
+@router.post("/channels/{channel_id}/courses", response_model=CourseReadSchema, status_code=status.HTTP_201_CREATED)
 async def create_course(
     channel_id: str,
     course_data: CourseCreateSchema,
@@ -47,16 +55,36 @@ async def create_course(
     course_service: CourseService = Depends(get_course_service),
     channel_service: ChannelService = Depends(get_channel_service),
 ):
-    channel_data = await channel_service.validate_owner(channel_id, user_data.id)
+    channel_data = await channel_service.validate_owner(channel_id, user_data)
     return await course_service.create(course_data, channel_data, user_data)
 
-@router.delete("/{course_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/channels/{channel_id}/courses/{course_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_course(
     channel_id: str,
     course_id: UUID,
-    user: UserReadSchema = Depends(get_current_user),
+    user_data: UserReadSchema = Depends(get_current_user),
     course_service: CourseService = Depends(get_course_service),
     channel_service: ChannelService = Depends(get_channel_service),
 ):
-    channel_data = await channel_service.validate_owner(channel_id, user.id)
-    await course_service.delete_course(user, channel_data, course_id)
+    channel_data = await channel_service.validate_owner(channel_id, user_data)
+    await course_service.delete_course(user_data, channel_data, course_id)
+
+@router.patch("/channels/{channel_id}/courses/{course_id}", response_model=CourseReadSchema)
+async def patch_course(
+    channel_id: str,
+    course_id: UUID,
+    update_data: CourseUpdateSchema,
+    user_data: UserReadSchema = Depends(get_current_user),
+    course_service: CourseService = Depends(get_course_service),
+    channel_service: ChannelService = Depends(get_channel_service),
+):
+
+    await channel_service.validate_owner(channel_id, user_data)
+    return await course_service.update_course(course_id, update_data)
+
+@router.get("/channels/{channel_id}/courses", response_model=List[CourseReadSchema])
+async def get_channel_courses(
+    channel_id: str,
+    course_service: CourseService = Depends(get_course_service),
+):
+    return await course_service.get_courses_by_channel(channel_id)
