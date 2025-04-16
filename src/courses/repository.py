552be023a1ecb94729bc4e700315452b35b0ab12
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-04-16 18:19:38 +0300

    вынесена сущность структуры курсов как отдельный модуль

--- src/courses/repository.py
+++ src/courses/repository.py
@@ -5,7 +5,7 @@ from sqlalchemy import update,select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..core.AbstractRepository import AbstractRepository
-from .models import CoursesORM, CoursesStructureORM
+from .models import CoursesORM
 
 
 
@@ -43,41 +43,4 @@ class CourseRepository(AbstractRepository[CoursesORM]):
             (CoursesORM.name == course_name)
         )
         result = await self.session.execute(query)
-        return result.scalar_one_or_none()
-
-
-class CourseStructureRepository(AbstractRepository[CoursesStructureORM]):
-    def __init__(self, session: AsyncSession):
-        super().__init__(session, CoursesStructureORM)
-
-    async def get_by_id(self, entity_id: UUID) -> Optional[CoursesStructureORM]:
-        """Получение структурыкурса по его UUID"""
-        return await super().get_by_id(entity_id)
-
-    async def get_all(self, limit: int = 20) -> List[CoursesStructureORM]:
-        """Не имплементировано"""
-        return await NotImplementedError()
-
-    async def create(self, entity: CoursesStructureORM) -> CoursesStructureORM:
-        """Создание новой структуры курса"""
-        return await super().create(entity)
-
-    async def delete(self, entity: CoursesStructureORM) -> Optional[CoursesStructureORM]:
-        """Удаление существующей структуры курса"""
-        await super().delete(entity)
-
-    async def update_structure(
-        self,
-        old_entity: CoursesStructureORM,
-        new_entity: CoursesStructureORM
-    ) -> Optional[CoursesStructureORM]:
-        query = (
-            update(self.model)
-            .where(self.model.course_id == old_entity.course_id)
-            .values(course_structure=new_entity.structure)
-            .returning(self.model)
-        )
-        result = await self.session.execute(query)
-
-        await self.session.commit()
         return result.scalar_one_or_none()
---
+++ src/courses_structure/repository.py
@@ -0,0 +1,46 @@
+from uuid import UUID
+from typing import List, Optional
+
+from sqlalchemy import update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ..core.AbstractRepository import AbstractRepository
+from .models import  CoursesStructureORM
+
+
+
+class CourseStructureRepository(AbstractRepository[CoursesStructureORM]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session, CoursesStructureORM)
+
+    async def get_by_id(self, entity_id: UUID) -> Optional[CoursesStructureORM]:
+        """Получение структурыкурса по его UUID"""
+        return await super().get_by_id(entity_id)
+
+    async def get_all(self, limit: int = 20) -> List[CoursesStructureORM]:
+        """Не имплементировано"""
+        return await NotImplementedError()
+
+    async def create(self, entity: CoursesStructureORM) -> CoursesStructureORM:
+        """Создание новой структуры курса"""
+        return await super().create(entity)
+
+    async def delete(self, entity: CoursesStructureORM) -> Optional[CoursesStructureORM]:
+        """Удаление существующей структуры курса"""
+        await super().delete(entity)
+
+    async def update_structure(
+        self,
+        old_entity: CoursesStructureORM,
+        new_entity: CoursesStructureORM
+    ) -> Optional[CoursesStructureORM]:
+        query = (
+            update(self.model)
+            .where(self.model.course_id == old_entity.course_id)
+            .values(course_structure=new_entity.structure)
+            .returning(self.model)
+        )
+        result = await self.session.execute(query)
+
+        await self.session.commit()
+        return result.scalar_one_or_none()
