124a270b67597b6f27c036111d8f3aab1790b217
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-04-16 18:16:59 +0300

    изменено получение зависимости gt_current_course, использована реализация get_current_channel

--- src/courses/dependencies.py
+++ src/courses/dependencies.py
@@ -1,22 +1,35 @@
-from fastapi import Depends
+from uuid import UUID
+from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database import get_async_session
 
-from ..auth.dependencies import get_current_user
-from ..auth.schemas import UserReadSchema
 
-from .repository import CourseRepository, CourseStructureRepository
-from .service import CourseService, CourseStructureService
+from ..channels.dependencies import get_current_channel
+from ..channels.schemas import ChannelReadSchema
+
+from .repository import CourseRepository
+from .service import CourseService
+from .schemas import CourseReadSchema
 
 
 
 
-
-def get_course_service(session: AsyncSession = Depends(get_async_session)) -> CourseService:
+async def get_course_service(session: AsyncSession = Depends(get_async_session)) -> CourseService:
     repository = CourseRepository(session)
     return CourseService(repository)
 
-def get_course_structure_service(session: AsyncSession = Depends(get_async_session)) -> CourseStructureService:
-    repository = CourseStructureRepository(session)
-    return CourseStructureService(repository)
+
+async def get_current_course(
+    course_id: UUID,
+    channel: ChannelReadSchema = Depends(get_current_channel),
+    course_service: CourseService = Depends(get_course_service),
+) -> CourseReadSchema:
+    course = await course_service.repository.get_by_id(course_id)
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found")
+
+    if course.channel_id != channel.id:
+        raise HTTPException(status_code=403, detail="This course does not belong to this channel")
+
+    return CourseReadSchema.model_validate(course)
