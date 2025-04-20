10163270ff46331215442b715d66b75f34d9da4b
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-04-20 12:41:28 +0300

    обновлена orm и бд для прав пользователей

--- src/permissions/models.py
+++ src/permissions/models.py
@@ -23,25 +23,34 @@ class UsersPermissionsORM(Base):
     __tablename__ = "users_permissions"
 
     user_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), ForeignKey(UsersORM.id, ondelete='CASCADE'), primary_key=True
+        UUID(as_uuid=True),
+        ForeignKey(UsersORM.id, ondelete="CASCADE"),
+        primary_key=True
     )
-
     course_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), ForeignKey(CoursesORM.id, ondelete='CASCADE'), primary_key=True
+        UUID(as_uuid=True),
+        ForeignKey(CoursesORM.id, ondelete="CASCADE"),
+        primary_key=True
     )
 
     access_level: Mapped[AccessLevelEnum] = mapped_column(
-        PgEnum
-        (
-            AccessLevelEnum, 
-            name="access_level_enum", 
+        PgEnum(
+            AccessLevelEnum,
+            name="access_level_enum",
             create_type=False,
-            values_callable=lambda e: [field.value for field in e]
+            values_callable=lambda e: [f.value for f in e],
         ),
         nullable=False,
         default=AccessLevelEnum.STUDENT
     )
 
-    #TODO: Как быть с владельцем курса, он должен иметь постоянное право владеть курсом
-    expiration_date: Mapped[datetime | None]
+    granted_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=datetime.now(UTC)
+    )
+    expiration_date: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True
+    )
     
