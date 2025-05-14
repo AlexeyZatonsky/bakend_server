84eac690a8fe10f151e2f0fa923de9de49c59c33
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-05-14 20:25:59 +0300

    refactor: streamline avatar_uploaded method and improve key handling

--- src/webhooks/service.py
+++ src/webhooks/service.py
@@ -1,19 +1,15 @@
 from fastapi import Request
 from uuid import UUID
-
-from pathlib import Path
 from urllib.parse import unquote_plus
 
-
 from ..core.Enums.TypeReferencesEnums import ImageTypeReference, InvalidUploadMimeError
-
-
 from ..auth.service import AuthService
 from ..settings.config import WEBHOOK_ENV
 
-
-
 from .schemas import MinioWebhookPayloadSchema
+
+from ..aws.upload_key import UploadKey
+from ..aws.strategies import ObjectKind
 
 import logging
 from ..core.log import configure_logging
@@ -21,15 +17,17 @@ from ..core.log import configure_logging
 logger = logging.getLogger(__name__)
 configure_logging()
 
-class WebhooksService():
-    def __init__():
+
+class WebhooksService:
+    def __init__(self) -> None:
         pass
 
     async def avatar_uploaded(
-            payload: MinioWebhookPayloadSchema,
-            request: Request,
-            auth_service: AuthService
-    )-> None:
+        self,
+        payload: MinioWebhookPayloadSchema,
+        request: Request,
+        auth_service: AuthService
+    ) -> dict:
         token = request.headers.get("X-Minio-Webhook-Token")
         expected = WEBHOOK_ENV.MINIO_WEBHOOK_TOKEN
         if token != expected:
@@ -43,18 +41,17 @@ class WebhooksService():
                 bucket = rec.s3.bucket.name
                 user_id = UUID(bucket)
 
-                key = unquote_plus(rec.s3.object.key)
-                prefix, filename = key.split("/", 1)
-                if prefix != "other":
+                raw_key = unquote_plus(rec.s3.object.key)
+                parsed_key = UploadKey.from_s3(user_id=user_id, key=raw_key)
+                if parsed_key is None or parsed_key.kind != ObjectKind.PROFILE_AVATAR:
+                    logger.debug("Пропущен ключ: %s", raw_key)
                     continue
 
-                ext = Path(filename).suffix.lstrip(".").lower()
+                logger.debug(f"Тип полученный из minio: {parsed_key.ext}")
+                img_type = ImageTypeReference.from_ext(parsed_key.ext)
 
-                logger.debug(f"Тип полученный из minio  {ext}")
-                img_type = ImageTypeReference.from_ext(ext)
-
-                await auth_service.set_avatar_extension(user_id, img_type.mime)
-                logger.debug("set_image_extension(%s, %s) выполнен", user_id, img_type)
+                await auth_service.set_avatar_extension(parsed_key.user_id, img_type.mime)
+                logger.debug("set_avatar_extension(%s, %s) выполнен", parsed_key.user_id, img_type.mime)
 
             except Exception as e:
                 logger.warning("Webhook record skipped: %r (%s)", rec, e)
