4349e3be82716f2e2cbdef3e5f23964016631215
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-05-15 17:32:15 +0300

    рефракторинг и обобщение методов парсинга ключа

--- src/webhooks/service.py
+++ src/webhooks/service.py
@@ -1,9 +1,13 @@
 from fastapi import Request
 from uuid import UUID
 from urllib.parse import unquote_plus
+from typing import Callable, Awaitable
 
 from ..core.Enums.TypeReferencesEnums import ImageTypeReference, InvalidUploadMimeError
+
 from ..auth.service import AuthService
+from ..channels.service import ChannelService
+
 from ..settings.config import WEBHOOK_ENV
 
 from .schemas import MinioWebhookPayloadSchema
@@ -18,9 +22,50 @@ logger = logging.getLogger(__name__)
 configure_logging()
 
 
+ParsedHandler = Callable[[UploadKey, str], Awaitable[None]]
+
 class WebhooksService:
     def __init__(self) -> None:
         pass
+
+    def check_token(self, request: Request) -> None:
+        token = request.headers.get("X-Minio-Webhook-Token")
+        expected = WEBHOOK_ENV.MINIO_WEBHOOK_TOKEN
+        if token != expected:
+            logger.debug("Токен Webhook MINIO не валиден: %r != %r", token, expected)
+            # raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid webhook token")
+        logger.debug("Токен Webhook MINIO прошёл валидацию")
+
+
+    async def process_webhook(
+        self,
+        *,
+        payload: MinioWebhookPayloadSchema,
+        request: Request,
+        allowed_kind: ObjectKind,
+        handler: ParsedHandler
+    ) -> dict:
+        self.check_token(request)
+
+        for rec in payload.Records:
+            try:
+                bucket = rec.s3.bucket.name
+                user_id = UUID(bucket)
+                raw_key = unquote_plus(rec.s3.object.key)
+
+                parsed_key = UploadKey.from_s3(user_id=user_id, key=raw_key)
+                if parsed_key is None or parsed_key.kind != allowed_kind:
+                    logger.debug("Пропущен ключ: %s", raw_key)
+                    continue
+
+                img_type = ImageTypeReference.from_ext(parsed_key.ext)
+                await handler(parsed_key, img_type.mime)
+
+            except Exception as e:
+                logger.warning("Webhook record skipped: %r (%s)", rec, e)
+                raise
+
+        return {"status": "ok"}
 
     async def avatar_uploaded(
         self,
@@ -28,33 +73,29 @@ class WebhooksService:
         request: Request,
         auth_service: AuthService
     ) -> dict:
-        token = request.headers.get("X-Minio-Webhook-Token")
-        expected = WEBHOOK_ENV.MINIO_WEBHOOK_TOKEN
-        if token != expected:
-            logger.debug("Токен Webhook MINIO не валиден: %r != %r", token, expected)
-            # raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid webhook token")
+        return await self.process_webhook(
+            payload=payload,
+            request=request,
+            allowed_kind=ObjectKind.PROFILE_AVATAR,
+            handler=lambda key, mime: auth_service.set_avatar_extension(key.user_id, mime)
+        )
 
-        logger.debug("Токен Webhook MINIO Прошёл валидацию")
 
-        for rec in payload.Records:
-            try:
-                bucket = rec.s3.bucket.name
-                user_id = UUID(bucket)
+    async def channel_avatar_uploaded(
+        self,
+        payload: MinioWebhookPayloadSchema,
+        request: Request,
+        channel_service: ChannelService
+    ) -> dict:
+        async def handle_channel_avatar(key: UploadKey, mime: str):
+            assert key.channel_id is not None, f"Отсутствует channel_id в ключе: {key.key}"
+            await channel_service.set_avatar_extension(channel_id=key.channel_id, mime=mime)
 
-                raw_key = unquote_plus(rec.s3.object.key)
-                parsed_key = UploadKey.from_s3(user_id=user_id, key=raw_key)
-                if parsed_key is None or parsed_key.kind != ObjectKind.PROFILE_AVATAR:
-                    logger.debug("Пропущен ключ: %s", raw_key)
-                    continue
+        return await self.process_webhook(
+            payload=payload,
+            request=request,
+            allowed_kind=ObjectKind.CHANNEL_AVATAR,
+            handler=handle_channel_avatar
+        )
 
-                logger.debug(f"Тип полученный из minio: {parsed_key.ext}")
-                img_type = ImageTypeReference.from_ext(parsed_key.ext)
 
-                await auth_service.set_avatar_extension(parsed_key.user_id, img_type.mime)
-                logger.debug("set_avatar_extension(%s, %s) выполнен", parsed_key.user_id, img_type.mime)
-
-            except Exception as e:
-                logger.warning("Webhook record skipped: %r (%s)", rec, e)
-                raise
-
-        return {"status": "ok"}
