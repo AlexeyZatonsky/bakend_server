81a014212be0f39e507054aa1b9c99eb58fd5ba7
Alexey Zatonsky <67155220+AlexeyZatonsky@users.noreply.github.com>
2025-05-19 15:07:57 +0300

    Обновлены схемы для работы с видео: добавлены поля для URL видео и превью, изменены существующие поля для исключения расширений из сериализации. Реализованы методы для формирования полных URL на основе данных пользователя и канала. Добавлено логирование для отладки.

--- src/videos/schemas.py
+++ src/videos/schemas.py
@@ -1,10 +1,19 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, field_serializer
 from uuid import UUID
 
+from ..settings.config import S3_ENV
+
 from ..core.Enums.ExtensionsEnums import ImageExtensionsEnum, VideoExtensionsEnum
+
+
+import logging
+from ..core.log import configure_logging
+
+logger = logging.getLogger(__name__)
+configure_logging()
 
 
 class BaseVideoDataSchema(BaseModel):
@@ -13,9 +22,13 @@ class BaseVideoDataSchema(BaseModel):
     course_id: Optional[UUID] = Field(description="ID курса")
     channel_id: str = Field(description="ID канала")
     
-    video_ext: VideoExtensionsEnum = Field(description="Расширение видео")
-    preview_ext: Optional[ImageExtensionsEnum] = Field(description="Расширение превью видео")
+    video_url: str = Field(description="URL видео")
+    preview_url: Optional[str] = Field(description="URL превью видео")
     
+    video_ext: VideoExtensionsEnum = Field(description="Расширение видео", exclude=True)
+    preview_ext: Optional[ImageExtensionsEnum] = Field(description="Расширение превью видео", exclude=True)
+    
+    name: str = Field(description="Название видео")
     description: str = Field(description="Описание видео")
     is_free: bool = Field(description="Доступно для всех")
     is_public: bool = Field(description="Публичное видео")
@@ -23,10 +36,37 @@ class BaseVideoDataSchema(BaseModel):
     upload_date: datetime = Field(description="Дата загрузки видео")
     
     
+    
+    
 class VideoDataCreateSchema(BaseVideoDataSchema): pass
 
 class VideoDataReadSchema(BaseVideoDataSchema): 
     model_config = ConfigDict(from_attributes=True)
+    
+    @field_serializer("video_url", when_used="json")
+    def get_video_url(self, video_url: str) -> str | None:
+        logger.debug(f"video_url: {video_url}")
+        
+        ext_value = self.video_ext.value # mp4
+        base_url = S3_ENV.BASE_SERVER_URL
+        return f"{base_url}/minio/{self.user_id}/channels/{self.channel_id}/{self.id}/video.{ext_value}"
+    
+    @field_serializer("preview_url", when_used="json")
+    def _get_full_preview_url(self, preview_url) -> str | None:
+        logger.debug(f"preview_url: {preview_url}, preview_ext: {self.preview_ext}")
+        
+        # Если avatar_ext есть, используем его для формирования URL
+        if self.preview_ext:
+            if isinstance(self.preview_ext, ImageExtensionsEnum):
+                ext_value = self.preview_ext.value
+            else:
+                ext_value = self.preview_ext
+            
+            base_url = S3_ENV.BASE_SERVER_URL
+            return f"{base_url}/minio/{self.user_id}/channels/{self.channel_id}/{self.id}/preview.{ext_value}"
+        
+        return None
+    
     
 
 class VideoDataUpdateSchema(BaseModel):
